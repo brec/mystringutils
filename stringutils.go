d9645ed363bf972f495b354839a956a25814ad63
Brandon Recinos <brandon.recinos@oracle.com>
2018-08-27 10:18:52 -0600

    upper and lower formatter

---
+++ stringutils.go
@@ -0,0 +1,13 @@
+package mystringutils
+
+import "strings"
+
+// Upper returns the case of the given argument
+func Upper(s string) string {
+    return strings.ToUpper(s)
+}
+
+//Lower returns the lower of the given argument
+func Lower(s string) string {
+  return string.ToLower(s)
+}
